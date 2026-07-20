f24b54d8c508ed1b5c927a147ae098f095972e57
Huey T <hueyathotmail@gmail.com>
2026-07-20 18:27:54 -0500

    dynamic load recommended and suggested

--- app/(dashboard)/for-you/page.tsx
+++ app/(dashboard)/for-you/page.tsx
@@ -1,8 +1,9 @@
-import { FaCirclePlay } from "react-icons/fa6";
 import SectionTitle from "@/app/components/SectionTitle";
 import SectionSubTitle from "@/app/components/SectionSubTitle";
 import BookCard from "@/app/components/BookCard";
 import SelectedBook from "@/app/components/SelectedBook";
+import RecommendedBooks from "@/app/components/RecommendedBooks";
+import SuggestedBooks from "@/app/components/SuggestedBooks";
 
 export default function forYou () {
     return (
@@ -11,23 +12,11 @@ export default function forYou () {
                 <SectionTitle title={"Selected just for you"} />
                     <SelectedBook />
                 <SectionTitle title={"Recommended For You"} />
-                    <SectionSubTitle subTitle={"We think you'll like these"} />
-                    <div className="flex mb-[32px] gap-[4px]">
-                        <BookCard />
-                        <BookCard />
-                        <BookCard />
-                        <BookCard />
-                        <BookCard />
-                    </div>
+                <SectionSubTitle subTitle={"We think you'll like these"} />
+                    <RecommendedBooks />
                 <SectionTitle title={"Suggested Books"} />
                 <SectionSubTitle subTitle={"Browse those books"} />
-                <div className="flex mb-[32px] gap-[4px]">
-                    <BookCard />
-                    <BookCard />
-                    <BookCard />
-                    <BookCard />
-                    <BookCard />
-                </div>
+                    <SuggestedBooks />
             </div>
         </>
     )
--- app/components/BookCard.tsx
+++ app/components/BookCard.tsx
@@ -1,20 +1,46 @@
 import Link from "next/link";
 import { AiOutlineStar } from "react-icons/ai";
 import { TfiTime } from "react-icons/tfi";
+import Image from "next/image";
 
-export default function BookCard () {
-    return (
-        <Link href="/book">
-            <div className="bookCard max-w-[175px] max-h-[386] text-[14px] text-[#6b757b] m-[8px] mb-[40px] mt-[40px]">
-                <img width={172} height={172}></img>
-                <div className="bookTitle text-[16px] text-[#032b41] mb-[8px]">How to Win Friends and Influence People in the Digital Age</div>
-                <div className="authorName mb-[8px]">Dale Carnegie</div>
-                <div className="bookSubTitle mb-[8px] text-[#394547]">Time-tested advice for the digital age</div>
-                <div className="bookCard_bottom flex">
-                    <div className="duration flex items-center mr-[8px]"><TfiTime className="mr-[4px]" />03:24</div>
-                    <div className="flex items-center"><AiOutlineStar className="mr-[4px]" />4.4</div>
-                </div>
-            </div>
-        </Link>
-    )
+export default function BookCard({ bookData }) {
+    if (!bookData) return null;
+  return (
+    <Link href={`/book/${bookData.id}`}>
+      <div className="bookCard max-w-[175px] text-[14px] text-[#6b757b] mr-[40px] mb-[40px] mt-[40px]">
+        
+        <Image
+          src={bookData.imageLink}
+          width={172}
+          height={172}
+          alt={bookData.title}
+          className="mb-[8px]"
+        />
+
+        <div className="bookTitle text-[16px] text-[#032b41] mb-[8px]">
+          {bookData.title}
+        </div>
+
+        <div className="authorName mb-[8px]">
+          {bookData.author}
+        </div>
+
+        <div className="bookSubTitle mb-[8px] text-[#394547]">
+          {bookData.subTitle}
+        </div>
+
+        <div className="bookCard_bottom flex">
+          <div className="duration flex items-center mr-[8px]">
+            <TfiTime className="mr-[4px]" />
+            03:24 {/*where is the data?*/}
+          </div>
+
+          <div className="flex items-center">
+            <AiOutlineStar className="mr-[4px]" />
+            {bookData.averageRating}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 }
