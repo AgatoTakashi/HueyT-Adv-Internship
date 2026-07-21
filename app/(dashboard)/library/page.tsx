ff9836a587665e95cb51e440907b7cc819242cc4
Huey T <hueyathotmail@gmail.com>
2026-07-20 23:13:16 -0500

    fix vercel

--- app/(dashboard)/library/page.tsx
+++ app/(dashboard)/library/page.tsx
@@ -1,6 +1,6 @@
 import SectionTitle from "@/app/components/SectionTitle";
 import SectionSubTitle from "@/app/components/SectionSubTitle";
-// import BookCard from "@/app/components/BookCard";
+
 
 export default function library () {
     return (
@@ -8,15 +8,11 @@ export default function library () {
             <div className="library flex flex-col p-[32px] mt-[8px]">
                 <SectionTitle title={"Saved Books"} />
                 <SectionSubTitle subTitle={"1 item"} />
-                {/* <BookCard /> */}
+
                 <SectionTitle title={"Finished"} />
                 <SectionSubTitle subTitle={"13 item"} />
                 <div className="flex">
-                    {/* <BookCard />
-                    <BookCard />
-                    <BookCard />
-                    <BookCard />
-                    <BookCard /> */}
+
                 </div>
             </div>
         </>
